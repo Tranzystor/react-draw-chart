6980a3df42553bdd810ea02033efd7bcd852e04a
Tranzystor <tranzystor.github@gmail.com>
2019-01-31 10:30:36 +0100

    remove styleguist

--- rollup.config.js
+++ rollup.config.js
@@ -1,51 +1,45 @@
-import babel from 'rollup-plugin-babel'
-import commonjs from 'rollup-plugin-commonjs'
-import external from 'rollup-plugin-peer-deps-external'
-import postcss from 'rollup-plugin-postcss'
-import resolve from 'rollup-plugin-node-resolve'
-import url from 'rollup-plugin-url'
-import svgr from '@svgr/rollup'
-import { terser } from 'rollup-plugin-terser'
+import babel from "rollup-plugin-babel";
+import commonjs from "rollup-plugin-commonjs";
+import external from "rollup-plugin-peer-deps-external";
+import resolve from "rollup-plugin-node-resolve";
+import url from "rollup-plugin-url";
+import svgr from "@svgr/rollup";
+import { terser } from "rollup-plugin-terser";
 
-import pkg from './package.json'
+import pkg from "./package.json";
 
 export default {
-  input: 'src/lib/index.js',
+  input: "src/index.js",
   output: [
     {
       file: pkg.main,
-      format: 'cjs',
-      sourcemap: true,
+      format: "cjs",
+      sourcemap: true
     },
     {
       file: pkg.module,
-      format: 'es',
-      sourcemap: true,
-    },
+      format: "es",
+      sourcemap: true
+    }
   ],
   plugins: [
-    postcss({
-      plugins: [],
-      minimize: true,
-      sourceMap: 'inline',
-    }),
     external({
-      includeDependencies: false,
+      includeDependencies: false
     }),
     url(),
     svgr(),
     resolve(),
     babel({
       plugins: [
-        '@babel/plugin-proposal-object-rest-spread',
-        '@babel/plugin-proposal-optional-chaining',
-        '@babel/plugin-syntax-dynamic-import',
-        '@babel/plugin-proposal-class-properties',
-        'transform-react-remove-prop-types',
+        "@babel/plugin-proposal-object-rest-spread",
+        "@babel/plugin-proposal-optional-chaining",
+        "@babel/plugin-syntax-dynamic-import",
+        "@babel/plugin-proposal-class-properties",
+        "transform-react-remove-prop-types"
       ],
-      exclude: 'node_modules/**',
+      exclude: "node_modules/**"
     }),
     commonjs(),
-    terser(),
-  ],
-}
+    terser()
+  ]
+};
