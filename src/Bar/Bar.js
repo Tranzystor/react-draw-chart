3cd8ca8d45fd97727e3eb539721ad24afddf585e
Tranzystor <tranzystor.github@gmail.com>
2019-01-31 14:57:25 +0100

    display bars

--- src/Bar/Bar.js
+++ src/Bar/Bar.js
@@ -1,7 +1,63 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { scaleBand, scaleLinear } from 'd3-scale';
+import _ from 'lodash';
 
-const Bar = () => {
-  return <div>Bar</div>;
+// #6534ff
+// #bbc4ef
+const background = '#fccdd3';
+const barColor = '#62bcfa';
+
+const Bar = ({ data }) => {
+  const width = 600;
+  const height = 400;
+
+  const xScale = scaleBand()
+    .range([0, width])
+    .domain(data.map(elem => elem.key))
+    .padding(0.1);
+  const barWidth = xScale.bandwidth();
+
+  const maxValObj = _.maxBy(data, o => o.value);
+  const yScale = scaleLinear()
+    .range([height, 0])
+    .domain([0, maxValObj.value]);
+
+  const result = data.map(x => {
+    const xAttr = xScale(x.key);
+    const yAttr = yScale(x.value);
+    const barHeight = height - yAttr;
+
+    return (
+      <rect
+        key={x.key}
+        x={xAttr}
+        y={yAttr}
+        height={barHeight}
+        width={barWidth}
+        style={{ fill: barColor }}
+      />
+    );
+  });
+
+  return (
+    <svg width={width} height={height} style={{ background }}>
+      <g>{result}</g>
+    </svg>
+  );
+};
+
+Bar.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      key: PropTypes.string.isRequired,
+      value: PropTypes.number.isRequired
+    })
+  ).isRequired
+};
+
+Bar.defaultProps = {
+  data: []
 };
 
 export default Bar;
--- src/Bar/Bar.stories.js
+++ src/Bar/Bar.stories.js
@@ -2,4 +2,33 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import Bar from './Bar';
 
-storiesOf('Bar', module).add('Bar', () => <Bar />);
+const data = [
+  { key: 'E', value: 0.12702 },
+  { key: 'T', value: 0.09056 },
+  { key: 'A', value: 0.08167 },
+  { key: 'O', value: 0.07507 },
+  { key: 'I', value: 0.06966 },
+  { key: 'N', value: 0.06749 },
+  { key: 'S', value: 0.06327 },
+  { key: 'H', value: 0.06094 },
+  { key: 'R', value: 0.05987 },
+  { key: 'D', value: 0.04253 },
+  { key: 'L', value: 0.04025 },
+  { key: 'C', value: 0.02782 },
+  { key: 'U', value: 0.02758 },
+  { key: 'M', value: 0.02406 },
+  { key: 'W', value: 0.0236 },
+  { key: 'F', value: 0.02288 },
+  { key: 'G', value: 0.02015 },
+  { key: 'Y', value: 0.01974 },
+  { key: 'P', value: 0.01929 },
+  { key: 'B', value: 0.01492 },
+  { key: 'V', value: 0.00978 },
+  { key: 'K', value: 0.00772 },
+  { key: 'J', value: 0.00153 },
+  { key: 'X', value: 0.0015 },
+  { key: 'Q', value: 0.00095 },
+  { key: 'Z', value: 0.00074 }
+];
+
+storiesOf('Bar', module).add('Bar', () => <Bar data={data} />);
